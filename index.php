fe39b0b5cea06e0906e4381818eaefaf6dd1e2a2
Osvaldo Zakowicz <osvaldozakowicz@gmail.com>
2026-01-30 17:33:46 -0300

    estructura y estilos inicial para el frontend

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
+    <!-- styles -->
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <title>Devbuddy</title>
+</head>
+
+<body>
+    <main class="container">
+        <section class="title-content">
+            <h1>devbuddy</h1>
+            <span>tu compañero de programaci&oacute;n!</span>
+            <p>modelo: qwen2.5-coder:3b</p>
+        </section>
+        <section class="chat-content">
+            <!-- input -->
+            <form action="">
+                <textarea class="chat-input" name="" id=""></textarea>
+            </form>
+            <!-- output -->
+            <section class="chat-output">
+                <p class="question">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quidem nihil recusandae illum? Ratione molestias enim expedita quo, dolorem, minus sequi magnam sit repellendus officia, cupiditate aliquam excepturi nihil quas.</p>
+                <p class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quidem nihil recusandae illum? Ratione molestias enim expedita quo, dolorem, minus sequi magnam sit repellendus officia, cupiditate aliquam excepturi nihil quas.</p>
+                <p class="question">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quidem nihil recusandae illum? Ratione molestias enim expedita quo, dolorem, minus sequi magnam sit repellendus officia, cupiditate aliquam excepturi nihil quas.</p>
+                <p class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quidem nihil recusandae illum? Ratione molestias enim expedita quo, dolorem, minus sequi magnam sit repellendus officia, cupiditate aliquam excepturi nihil quas.</p>
+                <p class="question">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quidem nihil recusandae illum? Ratione molestias enim expedita quo, dolorem, minus sequi magnam sit repellendus officia, cupiditate aliquam excepturi nihil quas.</p>
+                <p class="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quidem nihil recusandae illum? Ratione molestias enim expedita quo, dolorem, minus sequi magnam sit repellendus officia, cupiditate aliquam excepturi nihil quas.</p>
+            </section>
+        </section>
+    </main>
+</body>
+
+</html>
